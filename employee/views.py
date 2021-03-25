3a0337200b76f76b8ccbe684a9b5468909bd2fa5
kunneltech <it@kunnel.in>
2021-03-25 16:22:19 +0530

    ..

--- employee/views.py
+++ employee/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from .models import employee
 from rest_framework import viewsets
 from .serializers import employee_serializer
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 
 class employeView(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     queryset = employee.objects.all()
     serializer_class = employee_serializer
--- task/views.py
+++ task/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from .models import Task
 from rest_framework import viewsets
 from .serializers import Task_serializer
+from rest_framework.permissions import IsAuthenticated
+
 # Create your views here.
 
 
 class TaskView(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     queryset = Task.objects.all()
     serializer_class = Task_serializer
